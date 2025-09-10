9fe5c7487a25832cc68cdc0781ea7382dec69393
Adryel0713 <clauber.adriel@gmail.com>
2025-09-10 12:31:33 -0300

    exercise_3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
-import Greeting from "./Components/Greeting";
-import ProductInfo from "./Components/ProductInfo";
+import ProductList from "./Components/ProductList";
+import UserList from "./Components/UserList";
 
 const App = () => {
-  
+  const userInfo = [
+    {
+      username: "HuXn",
+      email: "teste@gmail.com",
+      location: "USA",
+    },
+    {
+      username: "John",
+      email: "jd@gmail.com",
+      location: "Arab",
+    },
+    {
+      username: "Alex",
+      email: "alexmersion@gmail.com",
+      location: "India",
+    }
+  ]
   return (
-  <div>
-    <Greeting/>
-    <ProductInfo/>
-  </div>
+  <main>
+    {/* { userInfo.map(user => (
+      <ul key={user}>
+        <li>Username: {user.username}</li>
+        <li>Email: {user.email}</li>
+        <li>Location: {user.location}</li>
+      </ul>
+    ))} */}
+    {/* { userInfo.map(({username,email,location}) => (
+      <ul key={Math.random()}>
+        <li>{username}</li>
+        <li>{email}</li>
+        <li>{location}</li>
+      </ul>
+    ))} */}
+
+    <UserList/>
+    <br /> <br />
+    <ProductList/>
+  </main>
   )
 
 }
