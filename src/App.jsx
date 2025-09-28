c3f7ac7a6b85f0af055a9a742ddfb9478dd92a5a
Adryel0713 <clauber.adriel@gmail.com>
2025-09-28 19:08:51 -0300

    teste - async await

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import Switcher from "./Components/Switcher"
+import { useEffect, useState } from "react"
 
 const App = () => {
+
+  const [data,setData] = useState([])
+    
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+      const data = await response.json()
+      if(data && data.length) setData(data)
+    }
+
+    getData()
+  })
+
   return (
-    <section>
+    <div>
 
-      <Switcher />
+      <ul>
+        {data.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
 
-    </section>
+    </div>
   )
 }
 
