cac11bf91bb69cb4c28ab4ce8a0b999ad5cd18cd
Adryel0713 <clauber.adriel@gmail.com>
2025-10-10 16:33:22 -0300

    Ex: useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import UpdateUser from "./Components/UpdateUser"
-import UserProfile from "./Components/UserProfile"
-import { UserProvider } from "./UserContext"
+import { useReducer } from "react";
 
+
+const reducer = (state, action) => {
+  switch(action.type){
+    case "+":
+      return { ...state, count: state.count + 1}
+    case "-":
+      return { ...state, count: state.count - 1}
+    case "reset":
+      return { ...state, count: 0}
+
+    default:
+      return state;
+  }
+}
 const App = () => {
 
+  const initialValue = {count: 0}
+  const [state,dispatch] = useReducer(reducer, initialValue);
 
   return (
-  <UserProvider>
-    <UserProfile />
-    <UpdateUser />
-  </UserProvider>
+  <div>
+    <button onClick={() => dispatch({type: "+"})}>+</button>
+    <button onClick={() => dispatch({type: "-"})}>-</button>
+    <button onClick={() => dispatch({type: "reset"})}>reset</button>
+    <h1>Count: {state.count}</h1>
+  </div>
   )
 }
 
